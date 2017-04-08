0d4bc57808ba00ec093ef1c7d9aa322055f8a7f0
Sergio Guoz <sergioguoz1@gmail.com>
2017-04-08 16:34:14 -0600

    Finalizacion de Grafos

--- Practica2EDD/src/practica2edd/Practica2EDD.java
+++ Practica2EDD/src/practica2edd/Practica2EDD.java
@@ -5,14 +5,7 @@
  */
 package practica2edd;
 
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
+
 import javax.swing.JFrame;
 
 /**
